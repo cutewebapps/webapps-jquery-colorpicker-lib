f4e8f0cfd305430c434e04d9a17d0e0e5c170430
Victor Lopata <v.lopata@webcerebrium.com>
2012-11-20 07:50:32 -0500

    double // in static path correction

--- classes/JQueryColorPicker/helper/Head.php
+++ classes/JQueryColorPicker/helper/Head.php
@@ -5,9 +5,9 @@ class JQueryColorPicker_HeadHelper extends App_ViewHelper_Abstract
     public function head()
     {
             $this->getView()->broker()->headScript()->append(
-                $this->getView()->staticpath() . '/jquery-colorpicker/js/colorpicker.js', 'jquery' );
+                $this->getView()->staticpath() . 'jquery-colorpicker/js/colorpicker.js', 'jquery' );
             
             $this->getView()->broker()->headLink()->append(
-                $this->getView()->staticpath() . '/jquery-colorpicker/css/colorpicker.css' );
+                $this->getView()->staticpath() . 'jquery-colorpicker/css/colorpicker.css' );
     }
 }
